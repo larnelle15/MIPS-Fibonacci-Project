3c257c875b8667b966990fc52c5d0a16307c51ed
Larnelle Ankunda <larnelleankunda50@gmail.com>
2024-12-05 23:02:30 -0500

    Creating a prompt for data input/intro

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,1 +1,6 @@
+.data
+prompt: .asciiz "Enter an integer (N >= 25): "  
+error: .asciiz "Illegal Number!\n"            
+fibo_msg: .asciiz "Fibonacci Sequence:\n"     
+space: .asciiz " "                          
 
